7489efeae9fd9e6d1569baa7aa1d1373b30b3aa5
Hala AlArid <halaalarid99@gmail.com>
2022-10-23 17:36:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+const leastFavoriteCustomer = "bob";
+
+function upperCaseCustomerName(){
+
+    return customerName = "BOB"
+}
+
+function setBestCustomer(){
+
+    var BestCustomer = "not bob";
+
+}
+
+function overwriteBestCustomer(){
+    BestCustomer  = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = "Talia";
+}
+
